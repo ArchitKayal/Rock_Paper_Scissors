09a9b3de62c4fd6d7880e778f0bca6e701957042
Archit Kayal <architkayal@gmail.com>
2025-08-09 00:14:20 +0530

    add human choice function

--- script.js
+++ script.js
@@ -11,5 +11,16 @@ function getComputerChoice () {
     }
 }
 
+function getHumanChoice () {
+    let data = prompt("Enter 1: Rock, 2: Paper, 3: Scissor")
+    if(data === "1"){
+        return "Rock"
+    }else if(data === "2"){
+        return "Paper"
+    }else{
+        return "Scissor"
+    }
+}
 
 console.log(getComputerChoice());
+console.log(getHumanChoice());
